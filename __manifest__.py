569ff73046d7fdb9b5e802012c7f3f3931362c12
Osmin <osmincano@gmail.com>
2018-02-27 12:00:57 -0600

    Creación inicial del proyecto

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "Xetechs Example",
+    "version" : "1.0",
+    "depends" : ["sale"],
+    "category" : "Sale",
+    "description": """
+Modulo de Ejemplo.
+""",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data": ["views/sale_views.xml"],
+    "active": False,
+    "installable": True
+}
